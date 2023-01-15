5ac4be99ab5c0d0edaa81e4bed93c7321b50ff3e
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-15 11:29:40 +0100

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+    // make sure start <= end
+    let auxInt = 0;
+    if (startInt > endInt){
+        auxInt = endInt;
+        endInt = startInt;
+        startInt = auxInt;
+    }
+    
+    const errorText = 'ERROR';
 
+    // only number parameters
+    if (typeof startInt !== 'number' || typeof endInt !== 'number'){
+        return errorText;
+    }
+    // no negative numbers
+    if (startInt < 0 || endInt < 0){
+        return errorText;
+    }
+
+    let result = 0;
+
+    for (let i = startInt; i <= endInt; i++){
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
