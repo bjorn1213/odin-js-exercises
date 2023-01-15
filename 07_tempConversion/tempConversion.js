742529d233ec17ad664bf9612d1f6cac41f39568
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-15 11:37:58 +0100

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  celciusTemp = (fahrenheitTemp - 32) * (5/9);
+  return roundToSingleDecimal(celciusTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celciusTemp) {
+  fahrenheitTemp = celciusTemp * (9/5) + 32;
+  return roundToSingleDecimal(fahrenheitTemp);
+};
+
+const roundToSingleDecimal = function(num){
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
