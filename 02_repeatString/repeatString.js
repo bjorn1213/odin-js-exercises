77f88850d27397bca5a29b487b0353522123c304
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-15 11:14:23 +0100

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let returnString = '';
 
+    if (num < 0) {
+        returnString = 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++){
+            returnString += str;
+        }
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
