9a6396ad3c8dee6ba2f9c584fd3eb18bc410c662
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-15 11:18:17 +0100

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let strArray = str.split('');
+    let reversedArray = strArray.reverse();
+    let returnString = '';
 
+    for (char of reversedArray){
+        returnString += char;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
