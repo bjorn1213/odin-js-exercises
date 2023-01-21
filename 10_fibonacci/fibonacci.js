bad57d675631edbc3f5659499b15b7d57c3dc30d
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-21 13:28:00 +0100

    Finish ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
-
+let fibs = {
+    0: 1,
+    1: 1,
+    2: 1,
 };
+
+const fibonacci = function(num) {
+
+    // input check
+    if (typeof num === 'string'){
+        num = +num;
+    }
+    if (num < 0) {
+        return "OOPS"
+    }
+
+    let answer
+    
+    if (!fibs[num]) {
+        answer = fibonacci(num-1) + fibonacci(num-2);
+        fibs[num] = answer;
+    } else {
+        answer = fibs[num];
+    }
+    
+    return answer
+};
+
 
 // Do not edit below this line
 module.exports = fibonacci;
