5b99990bd58b6b477c5e73fdda78ba97f104d4d9
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-21 13:47:42 +0100

    Finish ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+
+    return peopleArray.reduce(
+        (oldestPerson, currentPerson) => {
+            return calcAge(oldestPerson) > calcAge(currentPerson) ? oldestPerson : currentPerson;
+        },
+        peopleArray[0]
+    )
 
 };
+
+function calcAge(person){
+    let age;
+    if (!person['yearOfDeath']){
+        age = new Date().getFullYear() - person['yearOfBirth'];
+    } else {
+        age = person['yearOfDeath'] - person['yearOfBirth'];
+    }
+
+    return age
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
