6174e53d59af2bc6da6c682c0223a9ec7a1992f4
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-21 13:18:51 +0100

    Finish ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strippedStr = stripper(str);
+    return isPalindrome(strippedStr)
 };
+
+function isPalindrome(str){
+    const len = str.length;
+    if (len <= 1) {
+        return true;
+    }
+
+    const first = str[0];
+    const last = str[len-1];
+    if (first === last) {
+        return isPalindrome(str.slice(1,-1));
+    } else {
+        return false;
+    }
+};
+
+function stripper(str){
+    let strippedStr = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+                        .replace(/\s{1,}/g,"")
+                        .toLowerCase();
+
+    return strippedStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
